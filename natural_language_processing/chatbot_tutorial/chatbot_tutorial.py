3a0af0742cdc83ec316fdd97e44a15afbfd04a79
misogil0116 <lack_un@yahoo.co.jp>
2018-10-03 14:44:14 +0900

    fix

--- natural_language_processing/chatbot_tutorial/chatbot_tutorial.py
+++ natural_language_processing/chatbot_tutorial/chatbot_tutorial.py
@@ -27,6 +27,10 @@ from io import open
 import itertools
 import math
 
+USE_CUDA = torch.cuda.is_available()
+device = torch.device("cuda" if USE_CUDA else "cpu")
+
+
 # Default word tokens
 PAD_token = 0  # Used for padding short sentences
 SOS_token = 1  # Start-of-sentence token
@@ -320,7 +324,7 @@ class Attn(torch.nn.Module):
       attn_energies = self.general_score(hidden, encoder_outputs)
     elif self.method == "concat":
       attn_energies = self.concat_score(hidden, encoder_outputs)
-    elif self.method = "dot":
+    elif self.method == "dot":
       attn_energies = self.dot_score(hidden, encoder_outputs)
     
     # Transpose max_length and batch_size dimensions
@@ -357,29 +361,29 @@ class LuongAttnDecoderRNN(nn.Module):
     self.out = nn.Linear(hidden_size, output_size)
     self.attn = Attn(attn_model, hidden_size)
 
-   def forward(self, input_step, last_hidden, encoder_outputs):
-      # Note: we run this one step (word) at a time
-      # Get embedding of current input word
-      embedded = self.embedding(input_step)
-      embedded = self.embedding_dropout(embedded)
+  def forward(self, input_step, last_hidden, encoder_outputs):
+    # Note: we run this one step (word) at a time
+    # Get embedding of current input word
+    embedded = self.embedding(input_step)
+    embedded = self.embedding_dropout(embedded)
 
-      # Forward through unidirectional GRU
-      rnn_output, hidden = self.gru(embedded, last_hidden)
-      # Calculate attention weights from the current GRU output
-      attn_weights = self.attn(rnn_output, encoder_outputs)
-      # Multiply attention weights to encoder outputs to get new "weighted sum" context vector
-      context = attn_weights.bmm(encoder_outputs.transpose(0, 1))
+    # Forward through unidirectional GRU
+    rnn_output, hidden = self.gru(embedded, last_hidden)
+    # Calculate attention weights from the current GRU output
+    attn_weights = self.attn(rnn_output, encoder_outputs)
+    # Multiply attention weights to encoder outputs to get new "weighted sum" context vector
+    context = attn_weights.bmm(encoder_outputs.transpose(0, 1))
       
-      # Concatenate weighted context vector and GRU output using Luong eq. 5
-      rnn_output = rnn_output.squeeze(0)
-      context = context.squeeze(1)
-      concat_input = torch.cat((rnn_output, context), 1)
-      concat_output = torch.tanh(self.concat(concat_input))
-      # Predict next word using Luong eq. 6
-      output = self.out(concat_output)
-      output = F.softmax(output, dim=1)
-      # Return output and final hidden state
-      return output, hidden    
+    # Concatenate weighted context vector and GRU output using Luong eq. 5
+    rnn_output = rnn_output.squeeze(0)
+    context = context.squeeze(1)
+    concat_input = torch.cat((rnn_output, context), 1)
+    concat_output = torch.tanh(self.concat(concat_input))
+    # Predict next word using Luong eq. 6
+    output = self.out(concat_output)
+    output = F.softmax(output, dim=1)
+    # Return output and final hidden state
+    return output, hidden    
 
 def maskNLLoss(inp, target, mask):
   nTotal = mask.sum()
@@ -543,3 +547,174 @@ def trainIters(model_name, voc, pairs, encoder, decoder, encoder_optimizer, deco
                 'embedding': embedding.state_dict()
           }, os.path.join(directory, '{}_{}.tar'.format(iteration, 'checkpoint')))
 
+"""
+Define Evaluation
+- Encoderを入力として与えたときに, Decoderがどういう振る舞いをして出力するかを決める必要がある
+- Greedy Decoding: decoderの出力の中で、もっともsoftmaxが高くなるものを選択する
+
+計算グラフ
+  1. Encoderへ入力文を渡す
+  2. Encoderの最終層の隠れ層をDecoderへ渡す
+  3. Decoderの最初の入力を<SOS>で渡す
+  4. 毎時1単語ずつ渡し, 
+    4.a Decoderを計算
+    4.b softmax計算, tokenとscoreを保持
+    4.c 4.bで保持したtokenを元に次のdecoderの出力を計算する
+"""
+class GreedySearchDecoder(nn.Module):
+  def __init__(self, encoder, decoder):
+    super(GreedySearchDecoder, self).__init__()
+    self.encoder = encoder
+    self.decoder = decoder
+
+  def forward(self, input_seq, input_length, max_length):
+    # Forward Encoder(1)
+    encoder_outputs, encoder_hidden = self.encoder(input_seq, input_length)
+
+    # set encoder's final hidden layer to be first hidden input to the decoder
+    decoder_hidden = encoder_hidden[:decoder.n_layers]
+
+    # Initialize decoder input with SOS token
+    decoder_input = torch.ones(1, 1, device=device, dtype=torch.long) * SOS_token
+    # Initialize tensors to append decoded words to
+    all_tokens = torch.zeros([0], device=device, dtype=torch.long)
+    all_scores = torch.zeros([0], device=device)
+    # Iteratively decode one word token at a time
+    for _ in range(max_length):
+      # Forward pass through decoder
+      decoder_output, decoder_hidden = self.decoder(decoder_input, decoder_hidden, encoder_outputs)
+      # Obtain most likely word token and its softmax score
+      decoder_scores, decoder_input = torch.max(decoder_output, dim=1)
+      # Record token and score
+      all_tokens = torch.cat((all_tokens, decoder_input), dim=0)
+      all_scores = torch.cat((all_scores, decoder_scores), dim=0)
+      # Prepare current token to be next decoder input (add a dimension)
+      """
+      torch.unsqueeze
+      特定の位置に1サイズ分次元を追加した新しいTensorを返す
+      """
+      decoder_input = torch.unsqueeze(decoder_input, 0)
+      # Return collections of word tokens and scores
+    return all_tokens, all_scores
+
+
+# Evaluate Text
+# 実際にBotとやりとりができるようにする : コピペ
+def evaluate(encoder, decoder, searcher, voc, sentence, max_length=MAX_LENGTH):
+    ### Format input sentence as a batch
+    # words -> indexes
+    indexes_batch = [indexesFromSentence(voc, sentence)]
+    # Create lengths tensor
+    lengths = torch.tensor([len(indexes) for indexes in indexes_batch])
+    # Transpose dimensions of batch to match models' expectations
+    input_batch = torch.LongTensor(indexes_batch).transpose(0, 1)
+    # Use appropriate device
+    input_batch = input_batch.to(device)
+    lengths = lengths.to(device)
+    # Decode sentence with searcher
+    tokens, scores = searcher(input_batch, lengths, max_length)
+    # indexes -> words
+    decoded_words = [voc.index2word[token.item()] for token in tokens]
+    return decoded_words
+
+
+def evaluateInput(encoder, decoder, searcher, voc):
+    input_sentence = ''
+    while(1):
+        try:
+            # Get input sentence
+            input_sentence = input('> ')
+            # Check if it is quit case
+            if input_sentence == 'q' or input_sentence == 'quit': break
+            # Normalize sentence
+            input_sentence = normalizeString(input_sentence)
+            # Evaluate sentence
+            output_words = evaluate(encoder, decoder, searcher, voc, input_sentence)
+            # Format and print response sentence
+            output_words[:] = [x for x in output_words if not (x == 'EOS' or x == 'PAD')]
+            print('Bot:', ' '.join(output_words))
+
+        except KeyError:
+            print("Error: Encountered unknown word.")
+
+
+"""
+Run Model
+  1. Run Training
+"""
+# Configure models
+model_name = 'cb_model'
+attn_model = 'dot'
+#attn_model = 'general'
+#attn_model = 'concat'
+hidden_size = 500
+encoder_n_layers = 2
+decoder_n_layers = 2
+dropout = 0.1
+batch_size = 64
+
+# Set checkpoint to load from; set to None if starting from scratch
+loadFilename = None
+checkpoint_iter = 4000
+#loadFilename = os.path.join(save_dir, model_name, corpus_name,
+#                            '{}-{}_{}'.format(encoder_n_layers, decoder_n_layers, hidden_size),
+#                            '{}_checkpoint.tar'.format(checkpoint_iter))
+
+
+# Load model if a loadFilename is provided
+if loadFilename:
+    # If loading on same machine the model was trained on
+    checkpoint = torch.load(loadFilename)
+    # If loading a model trained on GPU to CPU
+    #checkpoint = torch.load(loadFilename, map_location=torch.device('cpu'))
+    encoder_sd = checkpoint['en']
+    decoder_sd = checkpoint['de']
+    encoder_optimizer_sd = checkpoint['en_opt']
+    decoder_optimizer_sd = checkpoint['de_opt']
+    embedding_sd = checkpoint['embedding']
+    voc.__dict__ = checkpoint['voc_dict']
+
+
+print('Building encoder and decoder ...')
+# Initialize word embeddings
+embedding = nn.Embedding(voc.num_words, hidden_size)
+if loadFilename:
+    embedding.load_state_dict(embedding_sd)
+# Initialize encoder & decoder models
+encoder = EncoderRNN(hidden_size, embedding, encoder_n_layers, dropout)
+decoder = LuongAttnDecoderRNN(attn_model, embedding, hidden_size, voc.num_words, decoder_n_layers, dropout)
+if loadFilename:
+    encoder.load_state_dict(encoder_sd)
+    decoder.load_state_dict(decoder_sd)
+# Use appropriate device
+encoder = encoder.to(device)
+decoder = decoder.to(device)
+print('Models built and ready to go!')
+
+
+# Configure training/optimization
+clip = 50.0
+teacher_forcing_ratio = 1.0
+learning_rate = 0.0001
+decoder_learning_ratio = 5.0
+n_iteration = 4000
+print_every = 1
+save_every = 500
+
+# Ensure dropout layers are in train mode
+encoder.train()
+decoder.train()
+
+# Initialize optimizers
+print('Building optimizers ...')
+encoder_optimizer = optim.Adam(encoder.parameters(), lr=learning_rate)
+decoder_optimizer = optim.Adam(decoder.parameters(), lr=learning_rate * decoder_learning_ratio)
+if loadFilename:
+    encoder_optimizer.load_state_dict(encoder_optimizer_sd)
+    decoder_optimizer.load_state_dict(decoder_optimizer_sd)
+
+# Run training iterations
+print("Starting Training!")
+trainIters(model_name, voc, pairs, encoder, decoder, encoder_optimizer, decoder_optimizer,
+           embedding, encoder_n_layers, decoder_n_layers, save_dir, n_iteration, batch_size,
+           print_every, save_every, clip, corpus_name, loadFilename)
