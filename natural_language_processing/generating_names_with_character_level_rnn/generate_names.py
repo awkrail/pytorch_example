3d9d8c626369c14e089a8e6397dbc984f7d05a0c
misogil0116 <lack_un@yahoo.co.jp>
2018-10-04 14:12:01 +0900

    add

--- natural_language_processing/generating_names_with_character_level_rnn/generate_names.py
+++ natural_language_processing/generating_names_with_character_level_rnn/generate_names.py
@@ -154,6 +154,9 @@ def train(category_tensor, input_line_tensor, target_line_tensor):
   loss.backward()
 
   for p in rnn.parameters():
+    """
+    optimを使えるように変更する
+    """
     p.data.add_(-learning_rate * p.grad.data)
   
   return output, loss.item() / input_line_tensor.size(0)
@@ -174,7 +177,43 @@ for iter in range(1, n_iter + 1):
 Plot the Losses
 """
 import matplotlib.pyplot as plt
-import matplotlib.ticker as ticker
 
 plt.figure()
-plt.plot(all_losses)
+plt.savefig("result.png")
+
+"""
+Sampling the network
+"""
+max_length = 20
+
+# Sample from a category and starting letter
+def sample(category, start_letter='A'):
+    with torch.no_grad():  # no need to track history in sampling
+        category_tensor = categoryTensor(category)
+        input = inputTensor(start_letter)
+        hidden = rnn.initHidden()
+
+        output_name = start_letter
+
+        for i in range(max_length):
+            output, hidden = rnn(category_tensor, input[0], hidden)
+            topv, topi = output.topk(1)
+            topi = topi[0][0]
+            if topi == n_letters - 1:
+                break
+            else:
+                letter = all_letters[topi]
+                output_name += letter
+            input = inputTensor(letter)
+
+        return output_name
+
+# Get multiple samples from one category and multiple starting letters
+def samples(category, start_letters='ABC'):
+    for start_letter in start_letters:
+        print(sample(category, start_letter))
+
+samples('Russian', 'RUS')
+samples('German', 'GER')
+samples('Spanish', 'SPA')
+samples('Chinese', 'CHI')
