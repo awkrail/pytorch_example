52df04a7c8c274899d1719cf944f8d54f0006f7b
misogil0116 <lack_un@yahoo.co.jp>
2018-10-05 14:29:08 +0900

    fix

--- natural_language_processing/classifying_names_with_a_character_level_rnn/classify_names.py
+++ natural_language_processing/classifying_names_with_a_character_level_rnn/classify_names.py
@@ -83,8 +83,8 @@ class RNN(nn.Module):
     super(RNN, self).__init__()
 
     self.hidden_size = hidden_size
-    self.i2o = nn.Linear(input_size + self.hidden_size, output_size)
-    self.i2h = nn.Linear(input_size + self.hidden_size, self.hidden_size)
+    self.i2o = nn.Linear(input_size + hidden_size, output_size)
+    self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
     self.softmax = nn.LogSoftmax(dim=1)
   
   def forward(self, input, hidden):
