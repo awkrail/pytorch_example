35c9861218a78c8aecd13aba96eeb3f5aa2d4587
misogil0116 <lack_un@yahoo.co.jp>
2018-10-07 16:20:45 +0900

    fix

--- natural_language_processing/classifying_names_with_a_character_level_rnn/classify_names.py
+++ natural_language_processing/classifying_names_with_a_character_level_rnn/classify_names.py
@@ -167,4 +167,41 @@ for iter in range(1, n_iters + 1):
 
 """
 EVALUATE THE RESULTS
+混合行列を作って評価
 """
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+
+confusion = torch.zeros(n_categories, n_categories)
+n_confusion = 10000
+
+def evaluate(line_tensor):
+  hidden = rnn.initHidden()
+
+  for i in range(line_tensor.size()[0]):
+    output, hidden = rnn(line_tensor[i], hidden)
+  
+  return output
+
+for i in range(n_confusion):
+  category, line, category_tensor, line_tensor = randomTrainingExample()
+  output = evaluate(line_tensor)
+  guess, guess_i = categoryFromOutput(output)
+  category_i = all_categories.index(category)
+  confusion[category_i][guess_i] += 1
+
+for i in range(n_categories):
+  confusion[i] = confusion[i] / confusion[i].sum()
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+cax = ax.matshow(confusion.numpy())
+fig.colorbar(cax)
+
+ax.set_xticklabels([''] + all_categories, rotation=90)
+ax.set_yticklabels([''] + all_categories)
+
+ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
+
+plt.savefig("result.png")
