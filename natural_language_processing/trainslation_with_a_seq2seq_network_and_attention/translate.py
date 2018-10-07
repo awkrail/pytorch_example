242a6667c1c1162861822e07b69fb8b89fc3cb1a
misogil0116 <lack_un@yahoo.co.jp>
2018-10-07 16:34:04 +0900

    add attention

--- natural_language_processing/trainslation_with_a_seq2seq_network_and_attention/translate.py
+++ natural_language_processing/trainslation_with_a_seq2seq_network_and_attention/translate.py
@@ -317,3 +317,82 @@ encoder1 = EncoderRNN(input_lang.n_words, hidden_size).to(device)
 attn_decoder1 = AttnDecoderRNN(hidden_size, output_lang.n_words, dropout_p=0.1).to(device)
 
 trainIters(encoder1, attn_decoder1, 75000, print_every=5000)
+
+"""
+EVALUATION
+"""
+def evaluate(encoder, decoder, sentence, max_length=MAX_LENGTH):
+  with torch.no_grad():
+    input_tensor = tensorFromSentence(input_lang, sentence)
+    input_length = input_tensor.size()[0]
+    encoder_hidden = encoder.initHidden()
+
+    encoder_outputs = torch.zero(max_length, encoder.hidden_size, device=device)
+
+    for ei in range(input_length):
+      encoder_output, encoder_hidden = encoder(input_tensor[ei],
+                                              encoder_hidden)
+      encoder_outputs[ei] += encoder_output[0, 0]
+    
+    decoder_input = torch.zero([[SOS_token]], device=device)
+    decoder_hidden = encoder_hidden
+
+    decoded_words = []
+    decoder_attentions = torch.zeros(max_length, max_length)
+
+    for di in range(max_length):
+      decoder_output, decoder_hidden, decoder_attention = decoder(
+        decoder_input, decoder_hidden, encoder_outputs
+      )
+      decoder_attentions[di] = decoder_attention.data # これは学習時は非推奨っぽい => detach使う
+      topv, topi = decoder.output.data.topk(1)
+
+      if topi.item() == EOS_token:
+        decoded_words.append("<EOS>")
+        break
+      else:
+        decoded_words.append(output_lang.index2word[topi.item()])
+      
+      # 次の入力は最も確率値が大きいものを利用する
+      decoder_input = topi.squeeze().detach()
+  return decoded_words, decoder_attentions[:di+1]
+
+# ランダムに出力して評価する。答えも見てみる
+def evaluateRandomly(encoder, decoder, n=10):
+    for i in range(n):
+        pair = random.choice(pairs)
+        print('>', pair[0])
+        print('=', pair[1])
+        output_words, attentions = evaluate(encoder, decoder, pair[0])
+        output_sentence = ' '.join(output_words)
+        print('<', output_sentence)
+        print('')
+
+
+# Visualizing Attention
+def showAttention(input_sentence, output_words, attentions):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    cax = ax.matshow(attentions.numpy(), cmap='bone')
+    fig.colorbar(cax)
+
+    ax.set_xticklabels([''] + input_sentence.split(' ') +
+                       ['<EOS>'], rotation=90)
+    ax.set_yticklabels([''] + output_words)
+
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+    ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
+
+    plt.savefig("attention_weights.png")
+
+def evaluateAndShowAttention(input_sentence):
+    output_words, attentions = evaluate(
+        encoder1, attn_decoder1, input_sentence)
+    print('input =', input_sentence)
+    print('output =', ' '.join(output_words))
+    showAttention(input_sentence, output_words, attentions)
+
+evaluateAndShowAttention("elle a cinq ans de moins que moi .")
+evaluateAndShowAttention("elle est trop petit .")
+evaluateAndShowAttention("je ne crains pas de mourir .")
+evaluateAndShowAttention("c est un jeune directeur plein de talent .")
